cd1de6c9b90e31191b16a9754d016a1db690b431
kalaivanan564 <kalaivananeswaran564@gmail.com>
2025-08-08 09:28:32 +0530

    dt

--- src/Movies/Movies.jsx
+++ src/Movies/Movies.jsx
@@ -14,3 +14,15 @@ let Movies = ()=>{
 }
 
 export default Movies;
+
+// import React from 'react'
+
+// class Movies extends React.Component{
+
+//     render(){
+//         return <div>
+//                     <h2>Movie Component</h2>
+//                 </div>
+//     }
+// }
+// export default Movies
